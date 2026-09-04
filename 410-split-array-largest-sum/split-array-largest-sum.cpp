060e092c5d21c43957f59f7bc3301aaf574c0da3
Nikita kumari <sinhanikita5r@gmail.com>
2026-09-04 12:01:27 +0530

    Time: 0 ms (100.00%) | Memory: 10.6 MB (43.86%) - LeetSync

--- 410-split-array-largest-sum/split-array-largest-sum.cpp
+++ 410-split-array-largest-sum/split-array-largest-sum.cpp
@@ -1,42 +1,34 @@
 class Solution {
 public:
-
-    int function(vector<int>& arr, int pages) {
-        int stu = 1;
-        int pageStudent = 0;
-
-        for(int i = 0; i < arr.size(); i++) {
-            if(pageStudent + arr[i] <= pages) {
-                pageStudent += arr[i];
-            }
-            else {
-                stu++;
-                pageStudent = arr[i];
-            }
+int function(vector<int>& arr, int pages){
+    int stu=1;
+    int NoPages=0;
+    for(int i=0; i<arr.size();i++){
+        if(NoPages+arr[i]<=pages){
+            NoPages+=arr[i];
         }
-
-        return stu;
+        else{
+            stu++;
+            NoPages=arr[i];
+        }
     }
+    return stu;
 
+}
     int splitArray(vector<int>& nums, int k) {
-
-        int low = *max_element(nums.begin(), nums.end());
-        int high = accumulate(nums.begin(), nums.end(), 0);
-
-        while(low <= high) {
-
-            int mid = low + (high - low) / 2;
-
-            int NoStu = function(nums, mid);
-
-            if(NoStu > k) {
-                low = mid + 1;
+        int low=*max_element(nums.begin(),nums.end());
+        int high=accumulate(nums.begin(),nums.end(),0);
+        while(low<=high){
+            int mid=(low+high)/2;
+           int Nostu=function(nums,mid);
+            if(Nostu>k){
+                low=mid+1;
             }
-            else {
-                high = mid - 1;
+            else{
+                high=mid-1;
             }
         }
-
         return low;
+        
     }
 };
